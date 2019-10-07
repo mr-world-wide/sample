59055b3a6bf2e696e0a1368053dd63066c646342
mr-world-wide <josh@tanda.co>
2019-10-06 23:56:33 -0500

    Convert to bucket lister

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+  get '/bucket-list-budgeting', to: 'static_pages#budget'
 
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
